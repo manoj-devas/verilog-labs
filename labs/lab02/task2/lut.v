e192e0e2ff3644e554bbaff6b292141fcb1b9ec4
Manoj Devasani <manojdevasani444@gmail.com>
2026-09-19 08:36:22 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -4,6 +4,7 @@
 //
 // YOU complete the two TODOs below. Everything else is given.
 
+
 module lut #(
   parameter WIDTH = 8,
   parameter DEPTH = 4
@@ -15,6 +16,17 @@ module lut #(
   reg [WIDTH-1:0] mem [0:DEPTH-1];
 
   integer i;
+  initial begin
+    for (i=0;i< DEPTH;i=i+1) begin
+      mem[i] = i*i;
+    end
+  end
+
+  always @(*) begin
+    dout = mem[sel];
+
+  end
+
 
   // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
   // Use an initial block with a for loop -- this is the only place a ROM's
--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,27 +1,43 @@
-// tb.v
-// Starter testbench template -- YOU complete this file.
-
 module tb;
 
-  // TODO: declare the inputs and outputs
+    parameter WIDTH = 8;
+    parameter DEPTH = 4;
 
-  // TODO: instantiate DUT here
+    reg  [$clog2(DEPTH)-1:0] t_sel;
+    wire [WIDTH-1:0] t_dout;
 
-  // Waveform dump configuration (DO NOT CHANGE)
-  string vcd_file;
-  initial begin
-    if ($value$plusargs("vcd=%s", vcd_file)) begin
-      $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+    integer i;
+
+    lut #(
+        .WIDTH(WIDTH),
+        .DEPTH(DEPTH)
+    ) DUT (
+        .sel(t_sel),
+        .dout(t_dout)
+    );
+
+    // Waveform dump configuration (DO NOT CHANGE)
+    string vcd_file;
+
+    initial begin
+        if ($value$plusargs("vcd=%s", vcd_file)) begin
+            $dumpfile(vcd_file);
+            $dumpvars(0, DUT);
+        end
     end
-  end
 
-  initial begin
-    // TODO: apply different input combinations
+    initial begin
+        for (i = 0; i < DEPTH; i = i + 1) begin
+            t_sel = i;
+            #5;
+        end
 
-  end
+        $finish;
+    end
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    initial begin
+        $monitor($time, " sel=%b | dout=%b (decimal %0d)",
+                 t_sel, t_dout, t_dout);
+    end
 
 endmodule
